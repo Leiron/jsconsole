c3a1c70e48fe0e5f683285d7f04b96807712aa5d
Leiron <phoenix4402@gmail.com>
2012-05-24 21:58:12 +0200

    REPLACE connect with express

--- server.js
+++ server.js
@@ -1,80 +1,87 @@
+
 // pending connect exposing static.mime (not available in npm yet)
-var mime = require('connect/node_modules/mime');
-mime.define({ 'text/cache-manifest': ['appcache'] });
 
-var connect = require('connect'),
-    parse = require('url').parse,
+var express     = require('express'),
+    app         = module.exports = express.createServer(),
+    parse       = require('url').parse,
     querystring = require('querystring').parse,
-    sessions = { run: {}, log: {} },
-    eventid = 0,
-    uuid = require('node-uuid');
+    sessions    = { run: {}, log: {} },
+    eventid     = 0,
+    uuid        = require('node-uuid');
 
-function remoteServer(app) {
-  app.get('/remote/:id?', function (req, res, next) {
-    var url = parse(req.url),
-        query = querystring(url.query);
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
 
-    // save a new session id - maybe give it a token back?
-    // serve up some JavaScript
-    var id = req.params.id || uuid();
-    res.writeHead(200, {'Content-Type': 'text/javascript'});
-    res.end((query.callback || 'callback') + '("' + id + '");');
-  });
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-  app.get('/remote/:id/log', function (req, res) {
-    var id = req.params.id;
-    res.writeHead(200, {'Content-Type': 'text/event-stream', 'Cache-Control': 'no-cache'});
-    res.write('eventId:0\n\n');
+app.configure('production', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler());
+});
 
-    sessions.log[id] = res;
-    sessions.log[id].xhr = req.headers['x-requested-with'] == 'XMLHttpRequest';
-  });
+app.get('/remote/:id?', function (req, res, next) {
+  var url = parse(req.url),
+      query = querystring(url.query);
 
-  app.post('/remote/:id/log', function (req, res) {
-    // post made to send log to jsconsole
-    var id = req.params.id;
-    // passed over to Server Sent Events on jsconsole.com
-    if (sessions.log[id]) {
-      sessions.log[id].write('data: ' + req.body.data + '\neventId:' + (++eventid) + '\n\n');
+  // save a new session id - maybe give it a token back?
+  // serve up some JavaScript
+  var id = req.params.id || uuid();
+  res.writeHead(200, {'Content-Type': 'text/javascript'});
+  res.end((query.callback || 'callback') + '("' + id + '");');
+});
 
-      if (sessions.log[id].xhr) {
-        sessions.log[id].end(); // lets older browsers finish their xhr request
-      }
+app.get('/remote/:id/log', function (req, res) {
+  var id = req.params.id;
+  res.writeHead(200, {'Content-Type': 'text/event-stream', 'Cache-Control': 'no-cache'});
+  res.write('eventId:0\n\n');
+
+  sessions.log[id] = res;
+  sessions.log[id].xhr = req.headers['x-requested-with'] == 'XMLHttpRequest';
+});
+
+app.post('/remote/:id/log', function (req, res) {
+  // post made to send log to jsconsole
+  var id = req.params.id;
+  // passed over to Server Sent Events on jsconsole.com
+  if (sessions.log[id]) {
+    sessions.log[id].write('data: ' + req.body.data + '\neventId:' + (++eventid) + '\n\n');
+
+    if (sessions.log[id].xhr) {
+      sessions.log[id].end(); // lets older browsers finish their xhr request
     }
+  }
 
-    res.writeHead(200, { 'Content-Type' : 'text/plain' });
-    res.end();
-  });
+  res.writeHead(200, { 'Content-Type' : 'text/plain' });
+  res.end();
+});
 
-  app.get('/remote/:id/run', function (req, res) {
-    var id = req.params.id;
-    res.writeHead(200, {'Content-Type': 'text/event-stream', 'Cache-Control': 'no-cache'});
-    res.write('eventId:0\n\n');
-    sessions.run[id] = res;
-    sessions.run[id].xhr = req.headers['x-requested-with'] == 'XMLHttpRequest';
-  });
+app.get('/remote/:id/run', function (req, res) {
+  var id = req.params.id;
+  res.writeHead(200, {'Content-Type': 'text/event-stream', 'Cache-Control': 'no-cache'});
+  res.write('eventId:0\n\n');
+  sessions.run[id] = res;
+  sessions.run[id].xhr = req.headers['x-requested-with'] == 'XMLHttpRequest';
+});
 
-  app.post('/remote/:id/run', function (req, res) {
-    var id = req.params.id;
+app.post('/remote/:id/run', function (req, res) {
+  var id = req.params.id;
 
-    if (sessions.run[id]) {
-      sessions.run[id].write('data: ' + req.body.data + '\neventId:' + (++eventid) + '\n\n');
+  if (sessions.run[id]) {
+    sessions.run[id].write('data: ' + req.body.data + '\neventId:' + (++eventid) + '\n\n');
 
-      if (sessions.run[id].xhr) {
-        sessions.run[id].end(); // lets older browsers finish their xhr request
-      }
+    if (sessions.run[id].xhr) {
+      sessions.run[id].end(); // lets older browsers finish their xhr request
     }
-    res.writeHead(200, { 'Content-Type' : 'text/plain' });
-    res.end();
-  });
-}
-
-// connect.static.mime.define('text/cache-manifest', ['appcache']);
-var server = connect.createServer()
-  .use(connect.bodyParser())
-  .use(connect.logger())
-  .use(connect.static(__dirname))
-  .use(remoteServer);
+  }
+  res.writeHead(200, { 'Content-Type' : 'text/plain' });
+  res.end();
+});
 
 console.log('Listening on ' + (process.argv[2] || 80));
-server.listen(parseInt(process.argv[2]) || 80);
+app.listen(parseInt(process.argv[2]) || 80);
